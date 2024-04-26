5e51761191db2a596ce0e4c8a6a0af0e5e76a518
mariope123 <167509355+mariope123@users.noreply.github.com>
2024-04-26 09:03:11 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+    basic.pause(2000)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+    basic.pause(2000)
+})
+basic.showString("PULSAR")
